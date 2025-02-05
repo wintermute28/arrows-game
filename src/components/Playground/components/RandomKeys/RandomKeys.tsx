33b69b4195d884e97aad5f2f856f0dc656752625
wintermute28 <a28c@yandex.ru>
2025-02-05 13:59:59 +0300

    ADD Controls and RandomKeys

---
+++ src/components/Playground/components/RandomKeys/RandomKeys.tsx
@@ -0,0 +1,26 @@
+import { useAppSelector } from "../../../../app/hooks"
+import { MAP_ARROW_CODES } from "../../constants"
+import type { IMapArrowCodes } from "../../types"
+// import styles from "./RandomKeys.module.css"
+
+export interface IRandomKeysProps {
+  isTimerActive: boolean
+}
+
+const RandomKeys: React.FC<IRandomKeysProps> = props => {
+  //   const { isTimerActive } = props
+
+  const state = useAppSelector(state => state.playground)
+
+  return (
+    <div>
+      {state.steps.map((element, index) => (
+        <span key={index}>
+          {MAP_ARROW_CODES[element.currentValue as keyof IMapArrowCodes]}
+        </span>
+      ))}
+    </div>
+  )
+}
+
+export default RandomKeys
--- src/components/Playground/store/slices.ts
+++ src/components/Playground/store/slices.ts
@@ -1,8 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit"
 import type { IPlaygroundState } from "./types"
+import { ARR_ARROW_CODES } from "../constants"
 
 export const initialState: IPlaygroundState = {
   currentStep: 0,
+  steps: [],
 }
 
 export const playgroundSlice = createSlice({
@@ -12,8 +14,16 @@ export const playgroundSlice = createSlice({
     setCurrentStep: state => {
       state.currentStep += 1
     },
+    setSteps: state => {
+      const randomKeys = Math.floor(Math.random() * ARR_ARROW_CODES.length)
+      console.log(ARR_ARROW_CODES[randomKeys])
+
+      state.steps.push({
+        currentValue: ARR_ARROW_CODES[randomKeys],
+      })
+    },
   },
 })
 
-export const { setCurrentStep } = playgroundSlice.actions
+export const { setCurrentStep, setSteps } = playgroundSlice.actions
 export default playgroundSlice.reducer
