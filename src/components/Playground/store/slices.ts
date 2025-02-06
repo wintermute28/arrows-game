37bc1b02588c30eadb7c2e1e2f990923193e3175
wintermute28 <a28c@yandex.ru>
2025-02-06 14:50:45 +0300

    Fix bug: multi-press correct keys

--- src/components/Playground/store/slices.ts
+++ src/components/Playground/store/slices.ts
@@ -1,12 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit"
-import type { IPlaygroundState } from "./types"
+
 import { ARR_ARROW_CODES } from "../constants"
+import type { IPlaygroundState } from "./types"
 
 export const initialState: IPlaygroundState = {
   currentStep: 0,
-  steps: [],
   totalSuccessful: 0,
   totalUnsuccessful: 0,
+  steps: [],
 }
 
 export const playgroundSlice = createSlice({
@@ -16,16 +17,18 @@ export const playgroundSlice = createSlice({
     setCurrentStep: state => {
       state.currentStep += 1
     },
+
     setSteps: state => {
-      const randomKeys = Math.floor(Math.random() * ARR_ARROW_CODES.length)
+      const randomKey = Math.floor(Math.random() * ARR_ARROW_CODES.length)
 
       state.steps.push({
         step: state.currentStep,
-        currentValue: ARR_ARROW_CODES[randomKeys],
+        currentValue: ARR_ARROW_CODES[randomKey],
         enteredValue: null,
         success: null,
       })
     },
+
     setEnteredValue: (state, action) => {
       if (state.steps.length) {
         const step = state.steps[state.currentStep - 1]
@@ -37,16 +40,17 @@ export const playgroundSlice = createSlice({
             enteredValue: action.payload,
             success: isSuccess,
           }
-        }
 
-        if (isSuccess) {
-          state.totalSuccessful += 1
-        } else {
-          state.totalUnsuccessful += 1
-          state.totalSuccessful = 0
+          if (isSuccess) {
+            state.totalSuccessful += 1
+          } else {
+            state.totalUnsuccessful += 1
+            state.totalSuccessful = 0
+          }
         }
       }
     },
+
     setUnsuccess: state => {
       if (state.steps.length) {
         const step = state.steps[state.currentStep - 1]
@@ -74,4 +78,5 @@ export const {
   setUnsuccess,
   resetStore,
 } = playgroundSlice.actions
+
 export default playgroundSlice.reducer
