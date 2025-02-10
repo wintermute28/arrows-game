abc4870490b4c985af045b38bf14572c24e31888
wintermute28 <a28c@yandex.ru>
2025-02-10 17:28:41 +0300

    Add styles for Playground and Score

--- src/components/Playground/components/Score/Score.tsx
+++ src/components/Playground/components/Score/Score.tsx
@@ -1,7 +1,9 @@
-// import styles from "./Score.module.css"
+import { Chip, Stack } from "@mui/material"
 
 import { useAppSelector } from "../../../../app/hooks"
 import { TypographyHeader, TypographyText } from "../../../UI"
+
+import styles from "./Score.module.css"
 
 const Score: React.FC = () => {
   const state = useAppSelector(state => state.playground)
@@ -13,9 +15,29 @@ const Score: React.FC = () => {
       <TypographyText>
         On error, the "Consecutive successful hits" value is reset to zero
       </TypographyText>
-      <span>Errors: {state.totalUnsuccessful}</span>
-      <br />
-      <span>Successful: {state.totalSuccessful}</span>
+
+      <Stack direction="row" spacing={1}>
+        <Chip
+          className={styles.chipUnsuccess}
+          label={
+            <>
+              Errors:{" "}
+              <span className={styles.counter}>{state.totalUnsuccessful}</span>
+            </>
+          }
+          variant="outlined"
+        />
+        <Chip
+          className={styles.chipSuccessful}
+          label={
+            <>
+              Successful:{" "}
+              <span className={styles.counter}>{state.totalSuccessful}</span>
+            </>
+          }
+          variant="outlined"
+        />
+      </Stack>
     </>
   )
 }
